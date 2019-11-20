a405fbf1e089097737fc858eb9b6593b1719bf51
Alexandre <contact@steptosuccess.fr>
2019-11-20 11:45:18 +0100

    ajout bundle api

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -4,11 +4,13 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
+ * @ApiResource
  */
 class Users implements UserInterface
 {
